d8bb7ee11485dbd046c113f915424a8db184fc49
clo3olb <clo3olb@gmail.com>
2024-04-10 23:59:09 +0800

    concise predict

--- prediction/predict.py
+++ prediction/predict.py
@@ -1,25 +1,30 @@
-from args import loadArgs
 import torch
 import numpy as np
 from models import PatchTST
 import os
+import pickle
+
+# './prediction/results/PatchTST_AAPL_336_96/args.pkl'
+# "./prediction/results/PatchTST_AAPL_336_96/checkpoints/checkpoint.pth"
 
 
-def predict(setting: str):
+def loadArgs(path: str):
+    with open(path, 'rb') as f:
+        args = pickle.load(f)
+        return args
 
-    # load args
-    args = loadArgs(setting)
+
+def predict(model_path: str, args_path: str, data: np.ndarray):
+
+    # Load model
+    args = loadArgs(args_path)
     model = PatchTST.Model(args).float()
-    path = os.path.join("./prediction/results/", setting, 'checkpoints')
-    best_model_path = path + '/' + 'checkpoint.pth'
+    path = os.path.join(model_path)
     model.load_state_dict(torch.load(
-        best_model_path, map_location=torch.device('cpu')))
-    batch_x = torch.rand(1, args.seq_len, 10).to(torch.device('cpu'))
+        path, map_location=torch.device('cpu')))
+
+    # Predict
+    batch_x = torch.tensor(data).float().to(torch.device('cpu'))
     outputs = model(batch_x)
     pred = outputs.detach().cpu().numpy()[0]  # .squeeze()
     return pred
-
-
-setting = 'PatchTST_AAPL_336_96'
-preds = predict(setting)
-print(preds.shape)
