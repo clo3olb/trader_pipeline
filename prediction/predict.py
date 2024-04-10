b35c55048a02e46d0a34eddc25c31cfd269ac1ad
clo3olb <clo3olb@gmail.com>
2024-04-10 23:34:51 +0800

    remove unused files

--- prediction/predict.py
+++ prediction/predict.py
@@ -1,55 +1,25 @@
 from args import loadArgs
 import torch
-from exp.exp_main import Exp_Main
-import random
 import numpy as np
+from models import PatchTST
+import os
 
 
-def predict(setting: str, data_path: str):
+def predict(setting: str):
 
     # load args
     args = loadArgs(setting)
-
-    # random seed
-    fix_seed = args.random_seed
-    random.seed(fix_seed)
-    torch.manual_seed(fix_seed)
-    np.random.seed(fix_seed)
-
-    # check GPU status and exit if not available
-    if not torch.cuda.is_available():
-        print('GPU is not available')
-        print("Use CPU for prediction")
-        # exit()
-
-    args.use_gpu = True if torch.cuda.is_available() and args.use_gpu else False
-
-    if args.use_gpu and args.use_multi_gpu:
-        args.dvices = args.devices.replace(' ', '')
-        device_ids = args.devices.split(',')
-        args.device_ids = [int(id_) for id_ in device_ids]
-        args.gpu = args.device_ids[0]
-
-    Exp = Exp_Main
-
-    # change data path to the one to predict
-    args.result_path = './prediction/results/'
-    args.root_path = './dataset/'
-    args.data_path = data_path
-    args.date_header = 'Timestamp'
-    args.batch_size = 1
-
-    exp = Exp(args)  # set experiments
-    print(
-        '>>>>>>>predicting : {}<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<'.format(setting))
-    preds = exp.predict(setting, True)
-
-    torch.cuda.empty_cache()
-
-    return preds
+    model = PatchTST.Model(args).float()
+    path = os.path.join("./prediction/results/", setting, 'checkpoints')
+    best_model_path = path + '/' + 'checkpoint.pth'
+    model.load_state_dict(torch.load(
+        best_model_path, map_location=torch.device('cpu')))
+    batch_x = torch.rand(1, args.seq_len, 10).to(torch.device('cpu'))
+    outputs = model(batch_x)
+    pred = outputs.detach().cpu().numpy()[0]  # .squeeze()
+    return pred
 
 
 setting = 'PatchTST_AAPL_336_96'
-data_path = '2022-01_AAPL.csv'
-preds = predict(setting, data_path)
-print(preds)
+preds = predict(setting)
+print(preds.shape)
